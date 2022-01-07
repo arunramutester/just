7880f49dd851648e16accc8426d83859e143abc1
arunramutester <arunramu092@gmail.com>
2022-01-07 08:12:05 +0530

    value

---
+++ src/org/git/OctoberBatch.java
@@ -0,0 +1,34 @@
+package org.git;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OctoberBatch {
+	
+	public static void main(String[] args) {
+		
+		//drivers and path
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\nice day\\eclipse-workspace\\GitProject\\Drivers\\chromedriver.exe");
+		
+		//launch browser
+		
+		WebDriver driver = new ChromeDriver();
+		
+		//pass URL
+		
+		driver.get("https://www.facebook.com/");
+		
+		//to find the locators
+		
+		driver.findElement(By.id("email")).sendKeys("arun");
+		
+		driver.findElement(By.id("pass")).sendKeys("arun@123");
+		
+		WebElement userlog = driver.findElement(By.xpath("//a[text()='Create New Account']"));
+		userlog.click();
+	}
+
+}
